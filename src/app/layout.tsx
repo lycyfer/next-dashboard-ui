954f611a4a57495ef11c343ca2ed8cbd816bfb57
Vitalik <gghjbhgfbb@gmail.com>
2025-05-02 14:39:38 +0300

    change title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
+  title: "vSchoolDashBoard",
   description: "Next.js School Management System",
 };
 
